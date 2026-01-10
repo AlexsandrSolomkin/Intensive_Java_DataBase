a8976db6fca22b97206cb45b71f51948d9b985a1
AlexsandrSolomkin <114250880+AlexsandrSolomkin@users.noreply.github.com>
2026-01-10 14:56:51 +0300

    Update Gateway, Service Discovery, Circuit Breaker, and External Config via Spring Cloud

--- target/generated-sources/annotations/com/example/userservice/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/userservice/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-12-28T19:16:24+0300",
+    date = "2026-01-09T18:18:38+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
